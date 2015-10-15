e7ac9d3ea274571534214f91b5bf17d323b8b6b2
Ben Hughes <git@mumble.org.uk>
2015-10-15 16:49:48 -0700

    Updated the mutt formula to pull in 1.5.24
    - Changed the format to match the main home in homebrew.
    - pulled in updated sidebar patch.
    - removed tls cipher patch as it's mainlined now.

--- mutt.rb
+++ mutt.rb
@@ -1,75 +1,93 @@
-require 'formula'
+# Note: Mutt has a large number of non-upstream patches available for
+# it, some of which conflict with each other. These patches are also
+# not kept up-to-date when new versions of mutt (occasionally) come
+# out.
+#
+# To reduce Homebrew's maintenance burden, new patches are not being
+# accepted for this formula. We would be very happy to see members of
+# the mutt community maintain a more comprehesive tap with better
+# support for patches.
 
 class Mutt < Formula
-  homepage 'http://www.mutt.org/'
-  url "ftp://ftp.mutt.org/mutt/mutt-1.5.23.tar.gz"
-  mirror "http://fossies.org/linux/misc/mutt-1.5.23.tar.gz"
-  sha1 "8ac821d8b1e25504a31bf5fda9c08d93a4acc862"
-  revision 2
+  desc "Mongrel of mail user agents (part elm, pine, mush, mh, etc.)"
+  homepage "http://www.mutt.org/"
+  url "https://bitbucket.org/mutt/mutt/downloads/mutt-1.5.24.tar.gz"
+  mirror "ftp://ftp.mutt.org/pub/mutt/mutt-1.5.24.tar.gz"
+  sha256 "a292ca765ed7b19db4ac495938a3ef808a16193b7d623d65562bb8feb2b42200"
 
-  head do
-    url 'http://dev.mutt.org/hg/mutt#HEAD', :using => :hg
-
-    resource 'html' do
-      url 'http://dev.mutt.org/doc/manual.html', :using => :nounzip
-    end
-
-    depends_on :autoconf
-    depends_on :automake
+  bottle do
+    sha256 "81c99d9cceb46d0c4c6f12aaceb29daa1e27aa83ef67c8201428e2757229b1e1" => :el_capitan
+    sha256 "9d83e71eeca14f5494a07abd68b6a723928cf415157dbf070461a10d0a0d89ae" => :yosemite
+    sha256 "28b3aa2d69d4eb12da355f7639c3e7eb4124337ff0c0d91477b4dd75c161ac67" => :mavericks
+    sha256 "3ed3daff645991c2f4a7f3eb91b6f65facced496e4d1aa28584f1cad29081763" => :mountain_lion
   end
 
+  head do
+    url "http://dev.mutt.org/hg/mutt#default", :using => :hg
+
+    resource "html" do
+      url "http://dev.mutt.org/doc/manual.html", :using => :nounzip
+    end
+  end
+
+  unless Tab.for_name("signing-party").with? "rename-pgpring"
+    conflicts_with "signing-party",
+      :because => "mutt installs a private copy of pgpring"
+  end
+
+  conflicts_with "tin",
+    :because => "both install mmdf.5 and mbox.5 man pages"
+
   option "with-debug", "Build with debug option enabled"
-  option "with-sidebar-patch", "Apply sidebar (folder list) patch" unless build.head?
-  option "with-trash-patch", "Apply trash folder patch"
-  option "with-slang", "Build against slang instead of ncurses"
+  option "with-s-lang", "Build against slang instead of ncurses"
   option "with-ignore-thread-patch", "Apply ignore-thread patch"
-  option "with-pgp-verbose-mime-patch", "Apply PGP verbose mime patch"
-  option "with-pgp-multiple-crypt-hook-patch", "Apply PGP multiple-crypti-hook patch"
   option "with-confirm-attachment-patch", "Apply confirm attachment patch"
-  option "with-tls-ciphers-patch", "Apply configure TLS ciphers patch"
   option 'with-hash-fingerprints-patch', 'Apply show more hashes on certs patch'
+  option "with-sidebar-patch", "Apply sidebar (folder list) patch" unless build.head?
 
-  depends_on 'tokyo-cabinet'
-  depends_on 's-lang' => :optional
-  depends_on 'openssl' if build.with? "tls-ciphers-patch" or build.with? 'hash-fingerprints-patch'
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
-  def patches
-    urls = [
-      ['with-sidebar-patch', 'https://raw.github.com/nedos/mutt-sidebar-patch/master/mutt-sidebar.patch'],
-      ['with-trash-patch', 'http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.4/features/trash-folder'],
-      # original source for this went missing, patch sourced from Arch at
-      # https://aur.archlinux.org/packages/mutt-ignore-thread/
-      ['with-ignore-thread-patch', 'https://gist.github.com/mistydemeo/5522742/raw/1439cc157ab673dc8061784829eea267cd736624/ignore-thread-1.5.21.patch'],
-      ['with-pgp-verbose-mime-patch',
-          'http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.2/features-old/patch-1.5.4.vk.pgp_verbose_mime'],
-      ['with-pgp-multiple-crypt-hook-patch',
-          'http://localhost.lu/mutt/patches/patch-1.5.22.sc.multiple-crypt-hook.1'],
-      ['with-pgp-combined-crypt-hook-patch',
-          'http://localhost.lu/mutt/patches/patch-1.5.22.sc.crypt-combined.1'],
-      ['with-confirm-attachment-patch', 'https://gist.github.com/tlvince/5741641/raw/c926ca307dc97727c2bd88a84dcb0d7ac3bb4bf5/mutt-attach.patch'],
-      ['with-tls-ciphers-patch', 'http://dev.mutt.org/trac/raw-attachment/ticket/3167/mutt-ssl-ciphers-patch.diff' ],
-      ['with-hash-fingerprints-patch', 'https://gist.githubusercontent.com/barn/a4ac8eda9592e1dd8b07/raw/a5e136e70ff6f00b32a5475799dd927e1c0b1b4e/mutt-hash-fingerprints-patch.diff']
-    ]
+  depends_on "openssl"
+  depends_on "tokyo-cabinet"
+  depends_on "s-lang" => :optional
+  # depends_on "gpgme" => :optional
 
-    if build.with? "ignore-thread-patch" and build.with? "sidebar-patch"
-      puts "\n"
-      onoe "The ignore-thread-patch and sidebar-patch options are mutually exlusive. Please pick one"
-      exit 1
+  # original source for this went missing, patch sourced from Arch at
+  # https://aur.archlinux.org/packages/mutt-ignore-thread/
+  if build.with? "ignore-thread-patch"
+    patch do
+      url "https://gist.githubusercontent.com/mistydemeo/5522742/raw/1439cc157ab673dc8061784829eea267cd736624/ignore-thread-1.5.21.patch"
+      sha256 "7290e2a5ac12cbf89d615efa38c1ada3b454cb642ecaf520c26e47e7a1c926be"
     end
+  end
 
-    p = []
-    urls.each do |u|
-      p << u[1] if build.include? u[0]
+  if build.with? "confirm-attachment-patch"
+    patch do
+      url "https://gist.githubusercontent.com/tlvince/5741641/raw/c926ca307dc97727c2bd88a84dcb0d7ac3bb4bf5/mutt-attach.patch"
+      sha256 "da2c9e54a5426019b84837faef18cc51e174108f07dc7ec15968ca732880cb14"
     end
+  end
 
-    return p
+  if build.with? 'hash-fingerprints-patch'
+    patch do
+      url 'https://gist.githubusercontent.com/barn/a4ac8eda9592e1dd8b07/raw/a5e136e70ff6f00b32a5475799dd927e1c0b1b4e/mutt-hash-fingerprints-patch.diff'
+      sha256 '548dd4631436984898bfcf5388937c7243eed5f6e7ffa09944e31baa861d342b'
+    end
+  end
+
+  if build.with? 'sidebar-patch'
+    patch do
+      url 'http://lunar-linux.org/~tchan/mutt/patch-1.5.24.sidebar.20150917.txt'
+      sha256 'ddc2baeb4d882ac32b5c54965dfb3a9b3164b2387888be33f4c1d16ebbea5b98'
+    end
   end
 
   def install
     args = ["--disable-dependency-tracking",
             "--disable-warnings",
             "--prefix=#{prefix}",
-            "--with-ssl",
+            "--with-ssl=#{Formula["openssl"].opt_prefix}",
             "--with-sasl",
             "--with-gss",
             "--enable-imap",
@@ -77,26 +95,27 @@ class Mutt < Formula
             "--enable-pop",
             "--enable-hcache",
             "--with-tokyocabinet",
-            # This is just a trick to keep 'make install' from trying to chgrp
-            # the mutt_dotlock file (which we can't do if we're running as an
-            # unpriviledged user)
+            # This is just a trick to keep 'make install' from trying
+            # to chgrp the mutt_dotlock file (which we can't do if
+            # we're running as an unprivileged user)
             "--with-homespool=.mbox"]
-    args << "--with-slang" if build.with? 's-lang'
+    args << "--with-slang" if build.with? "s-lang"
+    args << "--enable-gpgme" if build.with? "gpgme"
 
-    if build.with? 'debug'
+    if build.with? "debug"
       args << "--enable-debug"
     else
       args << "--disable-debug"
     end
 
-    if build.head?
-      system "./prepare", *args
-    else
-      system "./configure", *args
-    end
+    system "./prepare", *args
     system "make"
     system "make", "install"
 
-    (share/'doc/mutt').install resource('html') if build.head?
+    doc.install resource("html") if build.head?
+  end
+
+  test do
+    system bin/"mutt", "-D"
   end
 end
